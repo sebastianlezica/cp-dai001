7abae386612f3342cc3e4fbfcf5016780092f0f2
sebastianlezica <sebastianlezica@users.noreply.github.com>
2018-04-08 12:01:02 -0300

    Update DaiComplements.java

--- DaiComplements.java
+++ DaiComplements.java
@@ -6,6 +6,9 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.net.Uri;
+import android.os.PowerManager;
+import android.content.Context;
+import android.os.Build;
 
 public class DaiComplements extends CordovaPlugin {
     public DaiComplements() {}
@@ -15,11 +18,24 @@ public class DaiComplements extends CordovaPlugin {
                 cordova.getActivity().startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
                 callbackContext.success();
             } else if (action.equals("doze_request")) {
-                //cordova.getActivity().startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
+                /*
                 Intent intent = new Intent();
                 intent.setAction(android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
                 intent.setData(Uri.parse("package:" + cordova.getActivity().getPackageName()));
                 cordova.getActivity().startActivity(intent);
+                */
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    Intent intent = new Intent();
+                    String packageName = cordova.getActivity().getPackageName();
+                    PowerManager pm = (PowerManager)cordova.getActivity().getSystemService(Context.POWER_SERVICE);
+                    if (pm.isIgnoringBatteryOptimizations(packageName)) {
+                        intent.setAction(android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+                    } else {
+                        intent.setAction(android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                        intent.setData(Uri.parse("package:" + packageName));
+                    };
+                    cordova.getActivity().startActivity(intent);
+                };
                 callbackContext.success();
             } else {
                 callbackContext.error("Invalid action");
