2df3513e646b858049b4cbb98a6b3af87b9727ff
sebastianlezica <sebastianlezica@users.noreply.github.com>
2018-04-08 11:02:52 -0300

    Create DaiComplements.java

---
+++ DaiComplements.java
@@ -0,0 +1,26 @@
+package cordova.plugins;
+
+import android.content.Intent;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class DaiComplements extends CordovaPlugin {
+    public DaiComplements() {}
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        try {
+            if (action.equals("open")){
+                cordova.getActivity().startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
+                callbackContext.success();
+            }else {
+                callbackContext.error("Invalid action");
+                return false;
+            }
+        }catch(Exception e ) {
+            callbackContext.error("Exception occurred: ".concat(e.getMessage()));
+            return false;
+        }
+        return true;
+    }
+}
