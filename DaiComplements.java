4d1bacb48cf19a5b53078174e825d7a8102becb1
sebastianlezica <sebastianlezica@users.noreply.github.com>
2018-04-08 11:23:40 -0300

    Update DaiComplements.java

--- DaiComplements.java
+++ DaiComplements.java
@@ -13,7 +13,7 @@ public class DaiComplements extends CordovaPlugin {
             if (action.equals('settings_datetime_open')){
                 cordova.getActivity().startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
                 callbackContext.success();
-            if (action.equals('doze_request')){
+            } else if (action.equals('doze_request')){
                 cordova.getActivity().startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
                 callbackContext.success();
             } else {
