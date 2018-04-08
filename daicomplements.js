0cbd3836b249ecb2f78dd386131e32886c1d5730
sebastianlezica <sebastianlezica@users.noreply.github.com>
2018-04-08 11:27:13 -0300

    Update daicomplements.js

--- daicomplements.js
+++ daicomplements.js
@@ -2,10 +2,10 @@ var DaiComplements = (function(){
 
     DaiComplements = {
         settings_datetime_open: function(successFn, errorFn){
-            cordova.exec(successFn, errorFn, 'DaiComplements', 'settings_datetime_open', []);
+            cordova.exec(successFn, errorFn, "DaiComplements", "settings_datetime_open", []);
         },
         doze_request: function(successFn, errorFn){
-            cordova.exec(successFn, errorFn, 'DaiComplements', 'doze_request', []);
+            cordova.exec(successFn, errorFn, "DaiComplements", "doze_request", []);
         }
     };
     return DaiComplements;
